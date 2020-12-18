f057b02773de636c13b999f0f1fe2f3dd4090a14
Espen Klem <espen.klem@gmail.com>
2020-12-18 13:04:57 +0100

    Make Travis happy?

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,8 @@ module.exports = [
       fallback: {
         fs: false,
         path: require.resolve('path-browserify'),
-        stream: require.resolve('stream-browserify')
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/')
       }
     },
     node: {
