c988573e178e61a267c34a685f0af7e7b2a53c5d
Espen Klem <espen.klem@gmail.com>
2022-01-30 11:09:21 +0100

    Adding licenses header to minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import path from 'path'
+import license from 'rollup-plugin-license'
 
 export default [
   // browser-friendly UMD build
@@ -28,7 +30,15 @@ export default [
     plugins: [
       resolve(), // so Rollup can find `ms`
       commonjs(), // so Rollup can convert `ms` to an ES module
-      terser() // Minify
+      terser(), // Minify
+      license({ // Add reference to license file for minified scripts
+        banner: {
+          commentStyle: 'regular', // The default
+          content: {
+            file: path.join(__dirname, 'license-references.txt')
+          }
+        }
+      })
     ]
   }
 ]
